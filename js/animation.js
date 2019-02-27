46370121d74fecd9946aa65b434458cca4d4a7a2
ZacharyJose <e1646855@cmaisonneuve.qc.ca>
2019-02-27 13:37:24 -0500

    début animation 1

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', function(){
 		console.log('oui animation')
+		var titre1 = document.getElementsByClassName('site-title');
+		var titre2 = document.getElementsByClassName('site-description');
+
+		titre1[0].classList.add('animation1');
+		titre2[0].classList.add('animation2');
 })
